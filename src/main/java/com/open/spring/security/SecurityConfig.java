186135a9aa095d377853ae9524bcc6b092a09538
Rbojja23 <rbojja23@gmail.com>
2025-12-05 10:57:38 -0800

    fix: Add authentication requirement for `/api/grades/**` endpoints.

--- src/main/java/com/open/spring/security/SecurityConfig.java
+++ src/main/java/com/open/spring/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 * To enable HTTP Security in Spring
 */
 
-
 /*
  * THIS FILE IS IMPORTANT
  * 
@@ -50,80 +49,98 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 @Configuration
 public class SecurityConfig {
 
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtRequestFilter jwtRequestFilter;
+        private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+        private final JwtRequestFilter jwtRequestFilter;
 
-    // Inject the RateLimitFilter
-    private final RateLimitFilter rateLimitFilter;
+        // Inject the RateLimitFilter
+        private final RateLimitFilter rateLimitFilter;
 
-    public SecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                          JwtRequestFilter jwtRequestFilter,
-                          RateLimitFilter rateLimitFilter) {
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtRequestFilter = jwtRequestFilter;
-        this.rateLimitFilter = rateLimitFilter; 
-    }
+        public SecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                        JwtRequestFilter jwtRequestFilter,
+                        RateLimitFilter rateLimitFilter) {
+                this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+                this.jwtRequestFilter = jwtRequestFilter;
+                this.rateLimitFilter = rateLimitFilter;
+        }
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        @Bean
+        @Order(1)
+        public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .securityMatcher("/api/**", "/authenticate")
-                // JWT related configuration
-                .csrf(csrf -> csrf.disable())
-                // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) OBSOLETE, OVERWRITTEN BY BELOW
-                .authorizeHttpRequests(auth -> auth
+                http
+                                .securityMatcher("/api/**", "/authenticate")
+                                // JWT related configuration
+                                .csrf(csrf -> csrf.disable())
+                                // .sessionManagement(session ->
+                                // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) OBSOLETE,
+                                // OVERWRITTEN BY BELOW
+                                .authorizeHttpRequests(auth -> auth
 
+                                                // API
+                                                // ------------------------------------------------------------------------------
+                                                .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/analytics/**").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/person/**").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/person/{id}/balance").permitAll() // Allow
+                                                                                                                         // unauthenticated
+                                                                                                                         // access
+                                                                                                                         // to
+                                                                                                                         // this
+                                                                                                                         // endpoint
+                                                .requestMatchers(HttpMethod.GET, "/api/person/**").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/people/**").authenticated()
+                                                .requestMatchers(HttpMethod.POST, "/api/assets/upload").authenticated()
+                                                .requestMatchers(HttpMethod.GET, "/api/assets/upload/{id}")
+                                                .authenticated()
+                                                .requestMatchers(HttpMethod.GET, "/api/assets/uploads").authenticated()
+                                                .requestMatchers(HttpMethod.PUT, "/api/person/**").authenticated()
+                                                .requestMatchers(HttpMethod.DELETE, "/api/person/**")
+                                                .hasAuthority("ROLE_ADMIN")
 
-                        // API ------------------------------------------------------------------------------
-                        .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/analytics/**").permitAll()  
-                        .requestMatchers(HttpMethod.POST, "/api/person/**").permitAll()          
-                        .requestMatchers(HttpMethod.GET,"/api/person/{id}/balance").permitAll() // Allow unauthenticated access to this endpoint
-                        .requestMatchers(HttpMethod.GET, "/api/person/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/people/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/api/assets/upload").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/assets/upload/{id}").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/assets/uploads").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/api/person/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/api/person/**").hasAuthority("ROLE_ADMIN")
-                       
-                        .requestMatchers(HttpMethod.GET, "/api/plant/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/plant/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/plant/**").permitAll()
-                       
-                        .requestMatchers(HttpMethod.POST, "/api/synergy/grades/requests").hasAnyAuthority("ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/synergy/**").hasAnyAuthority("ROLE_TEACHER", "ROLE_ADMIN")
-                       
-                       
-                        .requestMatchers(HttpMethod.DELETE, "/api/synergy/saigai/").hasAnyAuthority("ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/calendar/add").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/calendar/add_event").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/calendar/edit/{id}").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/api/calendar/delete/{id}").permitAll()
-                   
-                        .requestMatchers(HttpMethod.GET,"/api/train/**").authenticated()
-                       
-                )
-                .cors(Customizer.withDefaults())
-                .headers(headers -> headers
-                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
-                        .addHeaderWriter(
-                                new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
-                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type",
-                                "Authorization", "x-csrf-token"))
-                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
-                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET",
-                                "DELETE", "OPTIONS", "HEAD")))
-                .exceptionHandling(exceptions -> exceptions
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                                                .requestMatchers(HttpMethod.GET, "/api/plant/**").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/plant/**").permitAll()
+                                                .requestMatchers(HttpMethod.PUT, "/api/plant/**").permitAll()
 
+                                                .requestMatchers(HttpMethod.POST, "/api/synergy/grades/requests")
+                                                .hasAnyAuthority("ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN")
+                                                .requestMatchers(HttpMethod.POST, "/api/synergy/**")
+                                                .hasAnyAuthority("ROLE_TEACHER", "ROLE_ADMIN")
 
-                // Session related configuration
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(rateLimitFilter, JwtRequestFilter.class);
+                                                .requestMatchers(HttpMethod.DELETE, "/api/synergy/saigai/")
+                                                .hasAnyAuthority("ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN")
+                                                .requestMatchers(HttpMethod.POST, "/api/calendar/add").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/calendar/add_event").permitAll()
+                                                .requestMatchers(HttpMethod.PUT, "/api/calendar/edit/{id}").permitAll()
+                                                .requestMatchers(HttpMethod.DELETE, "/api/calendar/delete/{id}")
+                                                .permitAll()
 
-        return http.build();
-    }
+                                                .requestMatchers(HttpMethod.GET, "/api/train/**").authenticated()
+                                                .requestMatchers("/api/grades/**").authenticated()
+
+                                )
+                                .cors(Customizer.withDefaults())
+                                .headers(headers -> headers
+                                                .addHeaderWriter(new StaticHeadersWriter(
+                                                                "Access-Control-Allow-Credentials", "true"))
+                                                .addHeaderWriter(
+                                                                new StaticHeadersWriter(
+                                                                                "Access-Control-Allow-ExposedHeaders",
+                                                                                "*", "Authorization"))
+                                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers",
+                                                                "Content-Type",
+                                                                "Authorization", "x-csrf-token"))
+                                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge",
+                                                                "600"))
+                                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods",
+                                                                "POST", "GET",
+                                                                "DELETE", "OPTIONS", "HEAD")))
+                                .exceptionHandling(exceptions -> exceptions
+                                                .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+
+                                // Session related configuration
+                                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                                .addFilterBefore(rateLimitFilter, JwtRequestFilter.class);
+
+                return http.build();
+        }
 }
